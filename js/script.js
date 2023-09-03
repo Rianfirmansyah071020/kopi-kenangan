2a60f41f4971b44c96303ed08bb62e974995be83
Rianfirmansyah071020 <rian071020@gmail.com>
2023-09-03 07:36:25 +0700

    selesai navbar

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ const navbarNav = document.querySelector(".navbar-nav");
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
+
+// klik diluar sidebar untuk menghilangkan nav
+
+let humberger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", (e) => {
+  if (!humberger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
